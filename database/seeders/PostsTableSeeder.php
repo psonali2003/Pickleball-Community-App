df794213b7ed7ce3571a2124c29818ab69b53a23
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:42:34 +0000

    Update PostsTableSeeder.php

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -8,15 +8,13 @@ use App\Models\Post;
 
 class PostsTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
+    
     public function run()
     {
+        
         User::all()->each(function ($user) {
             Post::factory()->count(3)->create(['user_id' => $user->id]);
+            
         });
     }
 }
