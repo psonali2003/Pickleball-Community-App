9a2b0d2188baec1733ac96d6d6d9a2d4b8fde5cf
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:49:31 +0000

    Update 2024_10_28_022417_create_events_table.php

--- database/migrations/2024_10_28_022417_create_events_table.php
+++ database/migrations/2024_10_28_022417_create_events_table.php
@@ -10,13 +10,14 @@ return new class extends Migration
 public function up()
 {
     Schema::create('events', function (Blueprint $table) {
+        
         $table->id();
         $table->foreignId('user_id')->constrained()->onDelete('cascade'); 
         $table->string('title');
         $table->text('description')->nullable();
         $table->dateTime('date'); 
         $table->string('location')->nullable(); 
-        $table->timestamps();
+        
     });
 }
 
