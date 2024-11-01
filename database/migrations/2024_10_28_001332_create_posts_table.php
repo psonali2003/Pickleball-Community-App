f0ea9fd7767192ed5033fe5f3604ca62d683a35a
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:24:55 +0000

    Update 2024_10_28_001332_create_posts_table.php

--- database/migrations/2024_10_28_001332_create_posts_table.php
+++ database/migrations/2024_10_28_001332_create_posts_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
@@ -19,11 +17,5 @@ return new class extends Migration
             $table->timestamps();
         });
     }
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('posts');
-    }
+ 
 };
