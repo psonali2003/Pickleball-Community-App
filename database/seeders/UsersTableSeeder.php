2fd18c43c39bd63ce074f3388228ecce2dfd108f
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:27:09 +0000

    Update UsersTableSeeder.php

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -7,9 +7,8 @@ use App\Models\User;
 
 class UsersTableSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+
+    
     public function run()
     {
         User::factory()->count(5)->create();
