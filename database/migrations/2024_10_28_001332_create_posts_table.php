35615812ad124c4610813c0ad98fb1ab09454886
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:50:14 +0000

    Update 2024_10_28_001332_create_posts_table.php

--- database/migrations/2024_10_28_001332_create_posts_table.php
+++ database/migrations/2024_10_28_001332_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('title');
             $table->text('body');
-            $table->timestamps();
+
         });
         
     }
