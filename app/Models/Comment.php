448b433e2c30de5fb984a43357c45953b1549c35
psonali2003 <81808722+psonali2003@users.noreply.github.com>
2024-11-01 14:51:20 +0000

    Update Comment.php

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,12 +8,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Comment extends Model
 {   
     use HasFactory;
+    
     public function user()
     {
         return $this->belongsTo(User::class);
+        
     }
     public function post()
     {
+
         
         return $this->belongsTo(Post::class);
     }
